7370c6a20cbf1bebbd78ff9c348cc50efb5fea8d
Jhonny <jhonnycgarcia@gmail.com>
2025-06-01 02:42:02 -0500

    Query Runner

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -20,7 +20,6 @@ export class ProductsController {
 
   @Get(':term')
   findOne(@Param('term') term: string) {
-    // return this.productsService.findOne(term);
     return this.productsService.findOnePlain(term);
   }
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, InternalServerErrorException, Logger, 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Product, ProductImage } from './entities';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
@@ -17,6 +17,7 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
     @InjectRepository(ProductImage)
     private readonly productImageRepository: Repository<ProductImage>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
@@ -84,15 +85,16 @@ export class ProductsService {
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
-    const product = await this.productRepository.preload({
-      id,
-      ...updateProductDto,
-      images: []
-    });
+    const { images = [], ...toUpdate } = updateProductDto;
+
+    const product = await this.productRepository.preload({id, ...toUpdate});
 
     if(!product) {
       throw new NotFoundException(`Product with id ${id} not found`);
     }
+
+    // Create query runner
+    const queryRunner = this.dataSource.createQueryRunner();
 
     try {
       await this.productRepository.save(product);
