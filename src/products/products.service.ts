711ebcd91b9f80b3bf8d85ac30882b36c59dec16
Jhonny <jhonnycgarcia@gmail.com>
2025-06-01 00:45:15 -0500

    Buscar por Slug o UUID

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -35,11 +36,17 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    let product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product | null;
+
+    if(isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
 
     if(!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with term ${term} not found`);
     }
 
     return product;
