c2de979f7b46168afe54240e81b9546f96b106c6
Jhonny <jhonnycgarcia@gmail.com>
2025-06-01 02:51:28 -0500

    Transacciones

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -95,12 +95,28 @@ export class ProductsService {
 
     // Create query runner
     const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
     try {
-      await this.productRepository.save(product);
-      return product;
+      if(images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+        product.images = images.map(
+          (image) => this.productImageRepository.create({ url: image })
+        );
+      }
+
+      await queryRunner.manager.save(product);
+
+      await queryRunner.commitTransaction();
+
+      return this.findOnePlain(id);
+
     } catch (error) {
+      await queryRunner.rollbackTransaction();
       this.handleDBExceptions(error);
+    } finally {
+      await queryRunner.release();
     }
   }
 
