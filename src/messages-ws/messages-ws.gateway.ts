b15b79ff2eab81dc9d709387bdfc1b1eddac57f8
Jhonny <jhonnycgarcia@gmail.com>
2025-06-03 17:57:53 -0500

    Server - Mantener identificados los clientes

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -10,10 +10,12 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   ) {}
   
   handleConnection(client: Socket) {
-    console.log('Client connected:', client.id);
+    this.messagesWsService.registerClient(client);
+    console.log('Client connected:', this.messagesWsService.getConnectedClients());
   }
   handleDisconnect(client: Socket) {
-    console.log('Client disconnected:', client.id);
+    this.messagesWsService.removeClient(client.id);
+    console.log('Client connected:', this.messagesWsService.getConnectedClients());
   }
 
 }
