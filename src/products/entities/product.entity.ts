5bb99e428c818662eb09212772a1b11d5f39b44b
Jhonny <jhonnycgarcia@gmail.com>
2025-06-03 13:58:58 -0500

    tags, apiproperty y apiresponse

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,54 +1,70 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 import { User } from "src/auth/entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({
     name: 'products'
 })
 export class Product {
+
+    @ApiProperty({
+        example: '123e4567-e89b-12d3-a456-426614174000',
+        description: 'Product ID',
+        uniqueItems: true,
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ApiProperty()
     @Column('text', {
         unique: true,
     })
     title: string;
 
+    @ApiProperty()
     @Column('float', {
         default: 0,
     })
     price: number;
 
+    @ApiProperty()
     @Column({
         type: 'text',
         nullable: true,
     })
     description: string;
 
+    @ApiProperty()
     @Column('text', {
         unique: true,
     })
     slug: string;
 
+    @ApiProperty()
     @Column('int', {
         default: 0,
     })
     stock: number;
 
+    @ApiProperty()
     @Column('text', {
         array: true,
     })
     sizes: string[];
 
+    @ApiProperty()
     @Column('text')
     gender: string;
 
+    @ApiProperty()
     @Column('text', {
         array: true,
         default: [],
     })
     tags: string[];
 
+    @ApiProperty()
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { SeedService } from './seed.service';
 import { Auth } from 'src/auth/decorators';
 import { ValidRoles } from 'src/auth/interfaces';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Seed')
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
