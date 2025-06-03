82999f54752019f792018a291c582163787a96d6
Jhonny <jhonnycgarcia@gmail.com>
2025-06-03 14:04:07 -0500

    Expandir el ApiProperty

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -16,55 +16,88 @@ export class Product {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'T-shirt Teslo',
+        description: 'Product Title',
+        uniqueItems: true,
+    })
     @Column('text', {
         unique: true,
     })
     title: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 10.99,
+        description: 'Product Price',
+        default: 0,
+    })
     @Column('float', {
         default: 0,
     })
     price: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        description: 'Product Description',
+        default: null,
+    })
     @Column({
         type: 'text',
         nullable: true,
     })
     description: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 't_shirt_teslo',
+        description: 'Product Slug',
+        uniqueItems: true,
+    })
     @Column('text', {
         unique: true,
     })
     slug: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 10,
+        description: 'Product Stock',
+        default: 0,
+    })
     @Column('int', {
         default: 0,
     })
     stock: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: ['M', 'L', 'XL', 'XXL'],
+        description: 'Product Sizes',
+    })
     @Column('text', {
         array: true,
     })
     sizes: string[];
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'men',
+        description: 'Product Gender',
+        enum: ['men', 'women', 'kid', 'unisex'],
+    })
     @Column('text')
     gender: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: ['shirt', 'pants', 'hat'],
+        description: 'Product Tags',
+    })
     @Column('text', {
         array: true,
         default: [],
     })
     tags: string[];
 
-    @ApiProperty()
+    @ApiProperty({
+        example: ['product_1.jpg', 'product_2.jpg'],
+        description: 'Product Images',
+    })
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
