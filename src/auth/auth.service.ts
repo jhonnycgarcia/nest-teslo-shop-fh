27fe77bf79c571b513220b628486e63a10d264ea
Jhonny <jhonnycgarcia@gmail.com>
2025-06-02 22:39:23 -0500

    Tarea: Cambiar email por id en el Payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,7 @@ export class AuthService {
 
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email }),
+        token: this.getJwtToken({ id: user.id }),
       };
       
     } catch (error) {
@@ -45,7 +45,7 @@ export class AuthService {
       const { email, password } = loginUserDto;
       const user = await this.userRepository.findOne({
         where: { email },
-        select: { email: true, password: true}
+        select: { email: true, password: true, id: true }
       });
       
       if(!user){
@@ -58,7 +58,7 @@ export class AuthService {
       
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email }),
+        token: this.getJwtToken({ id: user.id }),
       };
     } catch (error) {
       this.handleDBExceptions(error);
